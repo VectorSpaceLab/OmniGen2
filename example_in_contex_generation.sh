e53a9e0bffb96943a66b0357ac0c8aa18112cb2b
1224084650@qq.com <1224084650@qq.com>
2025-06-15 17:23:17 +0800

    update demo

--- example_in_contex_generation.sh
+++ example_in_contex_generation.sh
@@ -6,11 +6,11 @@ model_path="OmniGen2/OmniGen2"
 python inference.py \
 --model_path $model_path \
 --num_inference_step 50 \
---height 1536 \
+--height 1024 \
 --width 1024 \
 --text_guidance_scale 5.0 \
 --image_guidance_scale 2.0 \
---instruction "Change the background to classroom." \
---input_image_path example_images/ComfyUI_temp_mllvz_00071_.png \
---output_image_path outputs/output_edit.png \
+--instruction "Please let the person in image 2 hold the toy from the first image in a parking lot." \
+--input_image_path example_images/04.jpg example_images/000365954.jpg \
+--output_image_path outputs/output_in_context_generation.png \
 --num_images_per_prompt 1
